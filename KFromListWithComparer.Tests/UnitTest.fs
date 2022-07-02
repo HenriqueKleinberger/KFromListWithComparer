03677d38509c97908584a53ea5caa989bad78a34
Henrique Kleinberger <henriquedfkl@gmail.com>
2022-07-02 23:16:45 +0300

    tests added :white_check_mark:

--- KFromListWithComparer.Tests/UnitTest.fs
+++ KFromListWithComparer.Tests/UnitTest.fs
@@ -6,8 +6,13 @@ open Exercice
 [<TestFixture>]
 type TestClass () =
 
-    [<Test>]
-    member this.ShouldReturnTheKthFromTheListAfterListIsSorted() =
+    [<TestCase(1, 1)>]
+    [<TestCase(2, 2)>]
+    [<TestCase(3, 3)>]
+    [<TestCase(4, 4)>]
+    [<TestCase(5, 7)>]
+    [<TestCase(6, 9)>]
+    member this.ShouldReturnTheKthFromTheListAfterListIsSortedUsingIntComparer(index: int, expected: int) =
         let arg = [
             9
             3
@@ -17,19 +22,40 @@ type TestClass () =
             1
         ]
 
-        let rec quickSort list =
-          match list with
-          | [] -> []
-          | n::ns ->
-            let lessthan, greaterEqual = List.partition ((>) n) ns
-            quickSort lessthan @ n :: quickSort greaterEqual
+        let rec comparer (el1: int) (el2: int) = el1 >= el2
 
-        let result = getIndex 2 quickSort arg
+        let result = getIndex index comparer arg
 
-        Assert.AreEqual(2, result)
+        Assert.AreEqual(expected, result)
 
-    [<Test>]
-    member this.ShouldThrowErrorWhenIndexIsBiggerThanListLength() =
+    //[<Test>]
+    [<TestCase(1, "I")>]
+    [<TestCase(2, "hi")>]
+    [<TestCase(3, "dog")>]
+    [<TestCase(4, "code")>]
+    [<TestCase(5, "house")>]
+    [<TestCase(6, "israel")>]
+    [<TestCase(7, "company")>]
+    member this.ShouldReturnTheKthFromTheListAfterListIsSortedUsingStringComparer(index: int, expected: string) =
+        let arg = [
+            "israel"
+            "code"
+            "hi"
+            "I" 
+            "dog"
+            "house"
+            "company"
+        ]
+
+        let rec comparer (el1: string) (el2: string) = el1.Length >= el2.Length
+
+        let result = getIndex index comparer arg
+
+        Assert.AreEqual(expected, result)
+
+    [<TestCase(7)>]
+    [<TestCase(8)>]
+    member this.ShouldThrowErrorWhenIndexIsBiggerThanListLength(index: int) =
         let arg = [
             9
             3
@@ -39,18 +65,13 @@ type TestClass () =
             1
         ]
 
-        let rec quickSort list =
-          match list with
-          | [] -> []
-          | n::ns ->
-            let lessthan, greaterEqual = List.partition ((>) n) ns
-            quickSort lessthan @ n :: quickSort greaterEqual
+        let rec comparer (el1: int) (el2: int) = el1 >= el2
 
         
         let result = Assert.Throws<System.IndexOutOfRangeException>(fun () ->
-            getIndex 10 quickSort arg |> ignore
+            getIndex index comparer arg  |> ignore
         )
-        Assert.AreEqual("Index is Bigger than list lenght.", result.Message)
+        Assert.AreEqual("Index is Bigger than list length.", result.Message)
 
 
     
